055169c538e70d555ce89af277a9e2c66220b3b3
csyeah <tckand1533@gmail.com>
2023-03-05 10:13:48 +0800

    java finish

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -39,91 +39,108 @@ char stack::pop()
 }
 int main(void)
 {
-    
-    std::string line;
-    std::cout << "enter the postfix expression:";
-    getline(std::cin, line);
-
-
-    stack new_stack;
-    new_stack.initialize();
-
-    int len = line.size();
-    for (int i = 0; i < len; i++)
+    for(;;) 
     {
-        char pop_left, pop_right, tmp_num;
-        //std::cout << "deal with" << line[i] << std::endl;
-        switch (line[i])
+        
+        std::string line;
+        std::cout << "enter the postfix expression:";
+        getline(std::cin, line);
+
+
+        stack new_stack;
+        new_stack.initialize();
+
+        int len = line.size();//length of input string
+        for (int i = 0; i < len; i++)
         {
-        case '1':
-            new_stack.push('1' - '0');
-            break;
-        case '2':
-            new_stack.push('2' - '0');
-            break;
-        case '3':
-            new_stack.push('3' - '0');
-            break;
-        case '4':
-            new_stack.push('4' - '0');
-            break;
-        case '5':
-            new_stack.push('5' - '0');
-            break;
-        case '6':
-            new_stack.push('6' - '0');
-            break;
-        case '7':
-            new_stack.push('7' - '0');
-            break;
-        case '8':
-            new_stack.push('8' - '0');
-            break;
-        case '9':
-            new_stack.push('9' - '0');
-            break;
-        case '*':
-            pop_right = new_stack.pop();
-            pop_left = new_stack.pop();
-            new_stack.push(pop_left * pop_right);
-            break;
-        case '/':
-            pop_right = new_stack.pop();
-            pop_left = new_stack.pop();
-            new_stack.push(pop_left / pop_right);
-            break;
-        case '%':
-            pop_right = new_stack.pop();
-            pop_left = new_stack.pop();
-            new_stack.push(pop_left % pop_right);
-            break;
-        case '^':
-            pop_right = new_stack.pop();
-            pop_left = new_stack.pop();
-            tmp_num = pow(pop_left, pop_right);
-            new_stack.push(tmp_num);
-            break;
-        case '+':
-            pop_right = new_stack.pop();
-            pop_left = new_stack.pop();
-            new_stack.push(pop_left + pop_right);
-            break;
-        case '-':
-            pop_right = new_stack.pop();
-            pop_left = new_stack.pop();
-            new_stack.push(pop_left - pop_right);
-            break;
-        case ' ':
-            break;
-        default://calculated digit
-            break;
+            char pop_left, pop_right, tmp_num;
+            //std::cout << "deal with" << line[i] << std::endl;
+            switch (line[i])
+            {
+            case '1':
+                new_stack.push('1' - '0');
+                break;
+            case '2':
+                new_stack.push('2' - '0');
+                break;
+            case '3':
+                new_stack.push('3' - '0');
+                break;
+            case '4':
+                new_stack.push('4' - '0');
+                break;
+            case '5':
+                new_stack.push('5' - '0');
+                break;
+            case '6':
+                new_stack.push('6' - '0');
+                break;
+            case '7':
+                new_stack.push('7' - '0');
+                break;
+            case '8':
+                new_stack.push('8' - '0');
+                break;
+            case '9':
+                new_stack.push('9' - '0');
+                break;
+            case '*':
+                pop_right = new_stack.pop();
+                pop_left = new_stack.pop();
+                new_stack.push(pop_left * pop_right);
+                break;
+            case '/':
+                pop_right = new_stack.pop();
+                pop_left = new_stack.pop();
+                new_stack.push(pop_left / pop_right);
+                break;
+            case '%':
+                pop_right = new_stack.pop();
+                pop_left = new_stack.pop();
+                new_stack.push(pop_left % pop_right);
+                break;
+            case '^':
+                pop_right = new_stack.pop();
+                pop_left = new_stack.pop();
+                tmp_num = pow(pop_left, pop_right);
+                new_stack.push(tmp_num);
+                break;
+            case '+':
+                pop_right = new_stack.pop();
+                pop_left = new_stack.pop();
+                new_stack.push(pop_left + pop_right);
+                break;
+            case '-':
+                pop_right = new_stack.pop();
+                pop_left = new_stack.pop();
+                new_stack.push(pop_left - pop_right);
+                break;
+            case ' ':
+                break;
+            default:
+                break;
+            }
+        }
+        
+        //std::cout << "length of string:" << len << std::endl;
+        int ans = new_stack.pop();
+        std::cout << "the value of the expression is:" << ans << std::endl;
+        for (;;) {//ask if user wan to continue
+            std::cout << "continue?\n";
+            char option;
+            std::cin >> option;
+            if (option == 'Y' || option == 'y') {
+                std::cin.ignore(1024, '\n');//clear the buffer
+                break;
+            }
+            else if (option == 'N' || option == 'n')
+                goto exit;
+            else {
+                std::cout << "enter Y/y to continue or N/n to end\n";
+            }
         }
     }
-    
-    //std::cout << "length of string:" << len << std::endl;
-    int ans = new_stack.pop();
-    std::cout << "the value of the expression is:" << ans << std::endl;
-    //deal with 
-    return 0;
+    exit:
+        return 0;
 
 }
---
+++ hw1/hw1.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+
+class stack {
+    char[] stck = new char[100];
+    int top;
+    public void initialize() {
+        top = -1;
+    }
+    public void push(char ch) {
+        stck[++top] = ch;
+    }
+    public char pop() {
+        char tmp = stck[top--];
+        return tmp;
+    }
+}
+public class hw1 {
+    
+    public static void main(String[] args) {
+        
+        for (;;) {
+
+            String line;
+            stack new_stack = new stack();
+            new_stack.initialize();
+
+            Scanner input = new Scanner(System.in);
+            System.out.print("enter your expression:");
+            line = input.nextLine();
+            //char[] line_array = new char[line.length()];
+
+            for (int i = 0; i < line.length(); i++) 
+            {
+                char pop_left, pop_right, tmp_num;
+                if (line.charAt(i) <= '9' && line.charAt(i) >= '1')
+                    new_stack.push((char)(line.charAt(i) - '0'));
+                else
+                {
+                    switch (line.charAt(i)) {
+                        case '+':
+                            pop_right = new_stack.pop();
+                            pop_left = new_stack.pop();
+                            new_stack.push((char)(pop_left + pop_right));
+                            break;
+                        case '-':
+                            pop_right = new_stack.pop();
+                            pop_left = new_stack.pop();
+                            new_stack.push((char)(pop_left - pop_right));
+                            break;
+                        case '*':
+                            pop_right = new_stack.pop();
+                            pop_left = new_stack.pop();
+                            new_stack.push((char)(pop_left * pop_right));
+                            break;
+                        case '/':
+                            pop_right = new_stack.pop();
+                            pop_left = new_stack.pop();
+                            new_stack.push((char)(pop_left / pop_right));
+                            break;
+                        case '%':
+                            pop_right = new_stack.pop();
+                            pop_left = new_stack.pop();
+                            new_stack.push((char)(pop_left % pop_right));
+                            break;
+                        case '^':
+                            pop_right = new_stack.pop();
+                            pop_left = new_stack.pop();
+                            new_stack.push((char)Math.pow(pop_left, pop_right));
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            int ans = new_stack.pop();
+            System.out.println("the value of expression is:" + ans);
+            loops:
+                for (;;) {
+                    System.out.println("continue?");
+                    char opt;
+                    Scanner optScanner = new Scanner(System.in);
+                    opt = optScanner.next().charAt(0);
+                    if (opt == 'Y' || opt == 'y')
+                        break loops;
+                    else if (opt == 'N' || opt == 'n')
+                        System.exit(0);//end the program
+                    else
+                        continue loops;
+                }
+        }
+    }
+}
