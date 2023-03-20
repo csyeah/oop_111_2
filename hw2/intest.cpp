cc253b448199275ed02d34826199108e311193bc
csyeah <tckand1533@gmail.com>
2023-03-20 13:31:50 +0800

    test file input

---
+++ hw2/intest.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << "error" << std::endl;
+        exit(1);
+    }
+    std::ifstream in(argv[1]);
+    if (!in)
+    {
+        std::cout << "error" << std::endl;
+        exit(2);
+    }
+    //
+    std::cout << "yeah" << std::endl;
+    return 0;
+}
