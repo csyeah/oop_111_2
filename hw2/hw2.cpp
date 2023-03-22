f52641d17a2394f3ca591960c9d8ce62b5b12a35
csyeah <tckand1533@gmail.com>
2023-03-22 23:48:38 +0800

    finish

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -2,11 +2,30 @@
 #include<fstream>
 #include<string.h>
 #include<stdlib.h>
+#include<time.h>
 //input 9
 //every license 20 fish
 //1 fish per 1000 feet^3 (lake depth 20feet)
 //1/4 fish left
 const int LINE_LENGTH = 50;
+struct timespec diff(struct timespec start, struct timespec end)
+{
+    struct timespec tmp;
+    if ((end.tv_nsec - start.tv_nsec) < 0)
+    {
+        tmp.tv_sec = end.tv_sec - start.tv_sec - 1;
+        tmp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+    }
+    else
+    {
+        tmp.tv_sec = end.tv_sec - start.tv_sec;
+        tmp.tv_nsec = end.tv_nsec - start.tv_nsec;
+    }
+    return tmp;
+}
+
+
+
 int Split_array(int coordinate[10], char buf[LINE_LENGTH], int index_of_coordinate)//do strtok
 {
     char *token;
@@ -38,6 +57,9 @@ int Num_of_fishes(int coordinate[10])
 }
 int main(int argc, char *argv[])
 {
+    struct timespec time1, time2;
+    clock_gettime(CLOCK_MONOTONIC, &time1);//get the start time
+    
     if (argc < 2)
     {
         std::cout << "error, argc < 2" << std::endl;
@@ -54,10 +76,8 @@ int main(int argc, char *argv[])
     char data[10][LINE_LENGTH];
     int count_line = 0;
     while (file_in.getline(input_arr, LINE_LENGTH))
-    {
-        std::cout << "Read:" << input_arr << std::endl;
         strcpy(data[count_line++], input_arr);
-    }
+    
     std::cout << "read data from argv[1]" << std::endl;
     
     
@@ -71,15 +91,14 @@ int main(int argc, char *argv[])
         std::cout << "error, wrong number of coordinate" << std::endl;
         exit(3);
     }
-    for (int i = 0; i < index_of_coordinate; i++)
-        std::cout << coordinate[i] << " ";
-    
-    
-    std::cout << std::endl;
     
     int num_fish = Num_of_fishes(coordinate);
-   
+    int num_license = num_fish * 3 / 4 / 20;//calculate the maximun license
     
+    std::cout << "The maximun number of license can be sold is:" << num_license <<std::endl;
+    
+    clock_gettime(CLOCK_MONOTONIC, &time2);//get the end time
+    std::cout << "time spent=>" << diff(time1, time2).tv_sec << ":" << diff(time1, time2).tv_nsec << std::endl;
     
     return 0;
 }
