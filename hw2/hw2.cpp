482cf23fa4b673ee951f54c3a8871732826bc450
csyeah <tckand1533@gmail.com>
2023-03-22 23:20:50 +0800

    calculate number of fish

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -7,7 +7,7 @@
 //1 fish per 1000 feet^3 (lake depth 20feet)
 //1/4 fish left
 const int LINE_LENGTH = 50;
-int split_array(int coordinate[10], char buf[LINE_LENGTH], int index_of_coordinate)//do strtok
+int Split_array(int coordinate[10], char buf[LINE_LENGTH], int index_of_coordinate)//do strtok
 {
     char *token;
     token = strtok(buf, " ");
@@ -18,6 +18,23 @@ int split_array(int coordinate[10], char buf[LINE_LENGTH], int index_of_coordina
         token = strtok(NULL, " ");
     }
     return index_of_coordinate;
+}
+int Num_of_fishes(int coordinate[10])
+{
+    int sum = 0;
+    for (int i = 0; i < 9; i++)//calculate the area
+    {
+        if (i == 0 || i == 8)
+            sum += coordinate[i];
+        else if (i % 2 == 1)
+            sum += 4 * coordinate[i];
+        else
+            sum += 2 * coordinate[i];
+    }
+    sum = sum / 3 * 200 * 20;
+    int num_of_fish = sum / 1000;
+    return num_of_fish;//fish in lake
+    
 }
 int main(int argc, char *argv[])
 {
@@ -47,17 +64,20 @@ int main(int argc, char *argv[])
     
     int coordinate[9], index_of_coordinate = 0;
     for (int i = 0; i < count_line; i++)//convert string into int line by line 
-        index_of_coordinate = split_array(coordinate, data[i], index_of_coordinate);
+        index_of_coordinate = Split_array(coordinate, data[i], index_of_coordinate);
     
     if (index_of_coordinate != 9)//check if there is 9 input coordinate
+    {
         std::cout << "error, wrong number of coordinate" << std::endl;
-    
+        exit(3);
+    }
     for (int i = 0; i < index_of_coordinate; i++)
         std::cout << coordinate[i] << " ";
     
+    
     std::cout << std::endl;
     
-
+    int num_fish = Num_of_fishes(coordinate);
    
     
     
