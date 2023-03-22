3116b2a621de36e6e09fc102b9c6d949c276ba99
csyeah <tckand1533@gmail.com>
2023-03-22 23:54:04 +0800

    fix some detail

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -8,6 +8,7 @@
 //1 fish per 1000 feet^3 (lake depth 20feet)
 //1/4 fish left
 const int LINE_LENGTH = 50;
+//calculate the time program used
 struct timespec diff(struct timespec start, struct timespec end)
 {
     struct timespec tmp;
@@ -23,10 +24,8 @@ struct timespec diff(struct timespec start, struct timespec end)
     }
     return tmp;
 }
-
-
-
-int Split_array(int coordinate[10], char buf[LINE_LENGTH], int index_of_coordinate)//do strtok
+//use strtok to split array into int
+int Split_array(int coordinate[10], char buf[LINE_LENGTH], int index_of_coordinate)
 {
     char *token;
     token = strtok(buf, " ");
@@ -38,6 +37,7 @@ int Split_array(int coordinate[10], char buf[LINE_LENGTH], int index_of_coordina
     }
     return index_of_coordinate;
 }
+//calculate the number of fish
 int Num_of_fishes(int coordinate[10])
 {
     int sum = 0;
@@ -55,11 +55,14 @@ int Num_of_fishes(int coordinate[10])
     return num_of_fish;//fish in lake
     
 }
+
 int main(int argc, char *argv[])
 {
     struct timespec time1, time2;
     clock_gettime(CLOCK_MONOTONIC, &time1);//get the start time
     
+    std::cout << "start" << std::endl;
+    //read the file
     if (argc < 2)
     {
         std::cout << "error, argc < 2" << std::endl;
@@ -75,13 +78,12 @@ int main(int argc, char *argv[])
     char input_arr[LINE_LENGTH];
     char data[10][LINE_LENGTH];
     int count_line = 0;
-    while (file_in.getline(input_arr, LINE_LENGTH))
+    while (file_in.getline(input_arr, LINE_LENGTH))//read the file line by line until EOF
         strcpy(data[count_line++], input_arr);
     
-    std::cout << "read data from argv[1]" << std::endl;
+    std::cout << "read data from argv[1] successfully" << std::endl;
     
-    
-    
+    //get the coordinate
     int coordinate[9], index_of_coordinate = 0;
     for (int i = 0; i < count_line; i++)//convert string into int line by line 
         index_of_coordinate = Split_array(coordinate, data[i], index_of_coordinate);
@@ -92,6 +94,7 @@ int main(int argc, char *argv[])
         exit(3);
     }
     
+    //calculate the answer
     int num_fish = Num_of_fishes(coordinate);
     int num_license = num_fish * 3 / 4 / 20;//calculate the maximun license
     
