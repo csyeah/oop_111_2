3368fd116b523a235a1a37118bbc96ad7606d724
csyeah <tckand1533@gmail.com>
2023-03-23 20:37:36 +0800

    class init

--- hw3/triangle.h
+++ hw3/triangle.h
@@ -1,1 +1,17 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+
 #include<iostream>
+class Triangle
+{
+private:
+    const int length;
+    const char border_char;
+    const char fill_char;
+public:
+    Triangle(int input, char bor = '#', char fil = '*'):
+        length(input), border_char(bor), fill_char(fil) {}
+    ~Triangle();
+};
+
+#endif
