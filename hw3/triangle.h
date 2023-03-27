d309146428e5f2e5f1b3ffda5d3fcdf556aa1483
csyeah <tckand1533@gmail.com>
2023-03-27 13:33:33 +0800

    finish Draw()

--- hw3/triangle.h
+++ hw3/triangle.h
@@ -6,11 +6,20 @@ class Triangle
 {
 private:
     const int length;
-    const char border_char;
-    const char fill_char;
+    char border_char;
+    char fill_char;
 public:
     Triangle(int input, char bor = '#', char fil = '*'):
-        length(input), border_char(bor), fill_char(fil) {}
+        length(input > 39 ? 39: input < 1 ? 1: input), border_char(bor), fill_char(fil) {}
+    int GetSize();
+    int perimeter();
+    int Area();
+    void Draw();
+    void Grow();
+    void Shrink();
+    void Setborder();
+    void Setfill();
+    void Summary();
     ~Triangle();
 };
 
